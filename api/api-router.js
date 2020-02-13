f1d746331a06936340bfcabff69e02876576f582
Buddy Agyin <agyin3@gmail.com>
2020-02-12 20:57:17 -0700

    create routers

---
+++ api/api-router.js
@@ -0,0 +1,11 @@
+// Imports
+const router = require('express').Router()
+const authRouter = require('../auth/auth-router.js')
+const userRouter = require('../users/user-router.js')
+
+router.use('/auth', authRouter)
+router.use('/users', userRouter)
+
+router.get('/', (req,res) => {
+    res.json({api: 'Welcome to part town'})
+})
